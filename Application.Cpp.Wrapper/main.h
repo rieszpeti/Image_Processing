60a6286f39f51d00ea9b2c930ec1c0d4960aea29
SillySharp <rieszpeti@gmail.com>
2024-05-13 23:56:40 +0200

    remove unused project

--- Application.Cpp.Wrapper/main.h
+++
@@ -1,8 +0,0 @@
-class NativeClassWrapper {
-public:
-    unsigned char* ProcessImageWrapper(
-        unsigned char* img_pointer,
-        long data_len,
-        unsigned char* out_result,
-        int* length_of_out_result)
-};
